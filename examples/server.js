f9b363464d09c289e89352a28e2755faa53a87d8
daniel.murilo <daniel.murilo@superfestval.com.br>
2022-06-13 17:26:32 -0300

    deleted examples folder

--- examples/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-const router = require("./src/routes");
-
-app.use(express.json());
-app.use("/", router);
-
-app.listen(3000, () => console.log("running"));
